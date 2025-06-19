2d50555165e7f756f73651c913ae35916fd77037
Ozkar L. Garcell <ozkar.garcell@gmail.com>
2025-06-19 12:12:50 +0200

    feat: dev entrypoint script

--- conquistadores/manage.py
+++ conquistadores/manage.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
+
 import os
 import sys
+from pathlib import Path
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conquistadores.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.dev")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -15,8 +17,11 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    current_path = Path(__file__).parent.resolve()
+    sys.path.append(str(current_path / "conquistadores"))
+    execute_from_command_line(sys.argv)
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
