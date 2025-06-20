fe184dfb3e0eea05c21d56955a4a353042f89172
Ozkar L. Garcell <ozkar.garcell@gmail.com>
2025-06-20 17:06:45 +0200

    feat: added gallery model

--- conquistadores/apps/landing/models.py
+++ conquistadores/apps/landing/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from helpers.models import BaseModel
+from helpers.models import BaseModel, BaseTimeStampedModel
+from imagefield.fields import ImageField
 
 
 class AboutUs(BaseModel):
@@ -23,3 +24,28 @@ class AboutUsCard(BaseModel):
     class Meta:
         verbose_name = "Card Sección Quien Somos"
         verbose_name_plural = "Card Sección Quien Somos"
+
+
+class Gallery(BaseModel):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class GalleryImage(BaseTimeStampedModel):
+    gallery = models.ForeignKey(
+        Gallery, related_name="images", on_delete=models.CASCADE
+    )
+    image = ImageField(
+        upload_to="images/",
+        formats={
+            "thumb": ["default", ("crop", (300, 300))],
+            "desktop": ["default", ("thumbnail", (300, 225))],
+        },
+        auto_add_fields=True,
+    )
+    caption = models.CharField(
+        verbose_name="Descripción", max_length=200, blank=True
+    )
