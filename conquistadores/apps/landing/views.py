acffb4c3e23cc9d40f10ebb496b19db2b49142f6
Ozkar L. Garcell <ozkar.garcell@gmail.com>
2025-06-20 16:46:01 +0200

    feat: events and special events to landing

--- conquistadores/apps/landing/views.py
+++ conquistadores/apps/landing/views.py
@@ -1,8 +1,12 @@
 from typing import Any
 
+from apps.events.models import Events, SpecialEvents
 from apps.landing.models import AboutUs
+from django.utils import timezone
 from django.views.generic import TemplateView
 from helpers.mixins import HxTemplateMixin
+
+# today_start = datetime.combine(date.today(), datetime.min.time())
 
 
 class IndexView(HxTemplateMixin, TemplateView):
@@ -10,5 +14,16 @@ class IndexView(HxTemplateMixin, TemplateView):
 
     def get_context_data(self, **kwargs) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
-        context.update({"title": "Inicio", "about": AboutUs.objects.last()})
+        context.update(
+            {
+                "title": "Inicio",
+                "about": AboutUs.objects.last(),
+                "events": Events.objects.filter(
+                    start_time__gte=timezone.now()
+                ),
+                "special_events": SpecialEvents.objects.filter(
+                    date__gte=timezone.now()
+                ),
+            }
+        )
         return context
