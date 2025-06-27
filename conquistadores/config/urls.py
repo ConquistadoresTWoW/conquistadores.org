63550d59ca4298dd4a9a34a17c459b18c40270d3
Ozkar L. Garcell <ozkar.garcell@gmail.com>
2025-06-27 23:25:12 +0200

    fix: using server for static files

--- conquistadores/config/urls.py
+++ conquistadores/config/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path("ckeditor5/", include("django_ckeditor_5.urls")),
@@ -18,3 +19,16 @@ if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
     )
+else:
+    urlpatterns += [
+        re_path(
+            r"^media/(?P<path>.*)$",
+            serve,
+            {"document_root": settings.MEDIA_ROOT},
+        ),
+        re_path(
+            r"^static/(?P<path>.*)$",
+            serve,
+            {"document_root": settings.STATIC_ROOT},
+        ),
+    ]
