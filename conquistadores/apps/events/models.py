ebd1d824eb2d7beda98040910b17cf504e53c77c
Ozkar L. Garcell <ozkar.garcell@gmail.com>
2026-01-05 18:49:05 +0100

    feat/fix: changed models to new events

--- conquistadores/apps/events/models.py
+++ conquistadores/apps/events/models.py
@@ -1,66 +1,24 @@
 from django.db import models
-from helpers.models import BaseModel, BaseTimeStampedModel
-
-
-class Raids(BaseModel):
-    raid_id = models.IntegerField(
-        verbose_name="ID",
-    )
-    name = models.CharField(verbose_name="Raid", max_length=100)
-    reference = models.CharField(
-        verbose_name="Referencia", max_length=5, db_index=True
-    )
-    max_attendance = models.IntegerField(
-        verbose_name="Máximo de jugadores", default=10
-    )
-
-    class Meta:
-        verbose_name = "Raid"
-        verbose_name_plural = "Raids"
-
-    def __str__(self):
-        return self.name
+from helpers.models import BaseTimeStampedModel
 
 
 class Events(BaseTimeStampedModel):
-    class EventType(models.TextChoices):
-        RAID = "R", ("Raid")
-        PVP = "P", ("PVP")
-        SPECIAL = "E", ("Especial")
-
-    raid_id = models.ForeignKey(
-        Raids, on_delete=models.CASCADE, related_name="events"
+    event_id = models.CharField(
+        max_length=50, unique=True, db_index=True, null=True, blank=True
     )
-    event_id = models.IntegerField(verbose_name="ID", default=0)
-    type = models.CharField(
-        verbose_name="Tipo de Evento",
-        max_length=1,
-        choices=EventType,
-        default=EventType.RAID,
-        blank=True,
-        null=True,
+    title = models.CharField(
+        max_length=50, db_index=True, blank=True, null=True
     )
-    reference = models.CharField(
-        verbose_name="Referencia", max_length=6, db_index=True
-    )
-    description = models.CharField(
-        verbose_name="Descripción", max_length=150, null=True, blank=True
-    )
-    start_time = models.DateTimeField(verbose_name="Inicia")
-    lock_at_start_time = models.BooleanField(
-        verbose_name="Cierra al Iniciar", default=False
-    )
-    locked = models.BooleanField(verbose_name="Cerrada", default=False)
-    reservation_limit = models.IntegerField(
-        verbose_name="Límite de Reservas", default=1
-    )
-    allow_comments = models.BooleanField(default=True)
-    signups = models.IntegerField(verbose_name="Anotados", default=0)
+    leader = models.CharField(max_length=100, blank=True, null=True)
+    image = models.URLField(max_length=200, blank=True, null=True)
+    start = models.DateTimeField(db_index=True, blank=True, null=True)
+    attendees = models.IntegerField(default=0)
+    discord_channel_id = models.CharField(max_length=50, blank=True, null=True)
 
     class Meta:
         verbose_name = "Evento"
         verbose_name_plural = "Eventos"
-        ordering = ["start_time"]
+        ordering = ["start"]
 
     def __str__(self):
-        return self.reference
+        return self.title
